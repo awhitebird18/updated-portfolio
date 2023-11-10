077b7970b608961700d7ceec1fca56529e007a7e
Aaron <aaron.whitebird@gmail.com>
2023-11-09 21:10:43 -0500

    adjusted hero curve height

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -36,9 +36,9 @@ const Hero = ({ sectionRef }: { sectionRef: any }) => {
         <div className="hero-details w-11/12 md:w-3/5 min-h-[23rem] max-h-[28rem] absolute -bottom-12 md:-bottom-24 left-auto rounded-2xl overflow-hidden shadow-lg shadow-violet-500 max-w-5xl">
           <img src={sparxLogin} />
         </div>
-        <div className="w-screen h-min absolute bottom-0 left-0">
+        <div className="w-[102vw] h-min absolute bottom-0 -left-1">
           <img src={upcurve} alt="" className="w-full " />
-          <div className="h-32 md:h-12 bg-black"></div>
+          <div className="h-24 md:h-12 bg-black"></div>
         </div>
       </div>
     </FadeInContainer>
